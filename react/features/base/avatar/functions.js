4fa3df285bafd30ec27cc5dc100049af9905d5e5
Ilya Ostashkov <ostashkovilya@yandex.ru>
2020-05-12 16:51:15 +0300

    hide participans initials

--- react/features/base/avatar/functions.js
+++ react/features/base/avatar/functions.js
@@ -1,6 +1,6 @@
 // @flow
 
-import _ from 'lodash';
+// import _ from 'lodash';
 
 const AVATAR_COLORS = [
     '232, 105, 156',
@@ -41,14 +41,16 @@ export function getAvatarColor(initials: ?string) {
  * @returns {string?}
  */
 export function getInitials(s: ?string) {
-    // We don't want to use the domain part of an email address, if it is one
-    const initialsBasis = _.split(s, '@')[0];
-    const words = _.words(initialsBasis);
-    let initials = '';
+    // Ondoc: not show participant initials
+    // // We don't want to use the domain part of an email address, if it is one
+    // const initialsBasis = _.split(s, '@')[0];
+    // const words = _.words(initialsBasis);
+    // let initials = '';
 
-    for (const w of words) {
-        (initials.length < 2) && (initials += w.substr(0, 1).toUpperCase());
-    }
+    // for (const w of words) {
+    //     (initials.length < 2) && (initials += w.substr(0, 1).toUpperCase());
+    // }
 
-    return initials;
+    // return initials;
+    return s === null ? '' : '';
 }
